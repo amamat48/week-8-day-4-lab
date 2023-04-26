a366dd7a17cf5e195b28baea21810ffab7052356
Amari <amarimathis73@gmail.com>
2023-04-26 11:08:13 -0400

    finished

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import "./App.css"
 
 import { useState, useEffect } from 'react';
 import { getAllStarships } from './services/Sw-api';
+import Card from "./components/Card";
 
 function App() {
-  const [starships, setStarships] = useState([]);
+  const [starships, setStarships] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -17,11 +18,9 @@ function App() {
 
   return (
     <div className="starshipContainer">
-      {starships.map(starship => (
-        <div className="card" key={starship.url}>
-          <div className="cardBody">{starship.name}</div>
-        </div>
-      ))}
+      {starships? starships.map(starship => (
+        <Card props={starship}/>
+      )) : <div>loading...</div>}
     </div>
   );
 }
